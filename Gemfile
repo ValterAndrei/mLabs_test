2edd6f896f6bc33d6496629bfa9e7a8250869bb1
Valter Andrei <valterandrey@gmail.com>
2020-11-12 18:36:44 -0300

    Add rubocop

--- Gemfile
+++ Gemfile
@@ -3,10 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
-gem 'rails', '~> 6.0.3'
+gem 'bootsnap', '>= 1.4.2', require: false
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
-gem 'bootsnap', '>= 1.4.2', require: false
+gem 'rails', '~> 6.0.3'
 
 group :test do
   gem 'database_cleaner'
@@ -24,6 +24,7 @@ end
 
 group :development do
   gem 'listen', '~> 3.2'
+  gem 'rubocop', '~> 1.3', require: false
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
--- spec/use_cases/plate_validator_spec.rb
+++ spec/use_cases/plate_validator_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe PlateValidator do
   describe '.run' do
     context 'when plate is nil' do
       let(:plate) { nil }
-      
-      it { expect(described_class.new(plate).run).to be_falsey  }
+
+      it { expect(described_class.new(plate).run).to be_falsey }
     end
 
     context 'when the plate is correctly format' do
       let(:plate) { 'QBT-9377' }
-      
-      it { expect(described_class.new(plate).run).to be_truthy  }
+
+      it { expect(described_class.new(plate).run).to be_truthy }
     end
 
     context 'when the plate is incorrectly format' do
       let(:plate) { 'QBT-xxxx' }
-      
-      it { expect(described_class.new(plate).run).to be_falsey  }
+
+      it { expect(described_class.new(plate).run).to be_falsey }
     end
   end
 end
