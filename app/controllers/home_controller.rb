bbff0de350f135467ad68b2bdc8f5b5eeb998f20
Valter Andrei <valterandrey@gmail.com>
2020-11-12 18:53:30 -0300

    Refactor home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    validator = PlateValidator.new('bqf-9355').run
+    # validator = PlateValidator.new('bqf-9355').run
 
-    render json: validator, status: :ok
+    render json: 'Hello Valter!', status: :ok
   end
 end
