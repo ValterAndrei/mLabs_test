3d362eccaa3a2b95fa3c3977e7ca9fd0f6195cad
Valter Andrei <valterandrey@gmail.com>
2020-11-14 10:33:35 -0300

    Add validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,11 +5,27 @@ class Reservation < ApplicationRecord
 
   validates :checkin, presence: true
   validates :paid, :left, inclusion: [true, false]
+
   validate :can_reservate?, on: :create
+  validate :can_paid?,      on: :update
+  validate :can_left?,      on: :update
 
   private
 
   def can_reservate?
-    errors.add(:vehicle_id, 'has already reserved') if vehicle.reservations.exists?(left: false)
+    return unless vehicle.reservations.exists?(left: false)
+
+    errors.add(:vehicle, 'vehicle still in the parking lot')
+  end
+
+  def can_paid?
+    return if left
+    return if vehicle.reservations.exists?(paid: false)
+
+    errors.add(:paid, 'has already paid')
+  end
+
+  def can_left?
+    errors.add(:left, 'payment not yet made') unless paid
   end
 end
