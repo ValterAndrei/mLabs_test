a5a7acaa2a00c517f8963f073364515bcb1db8ec
Valter Andrei <valterandrey@gmail.com>
2020-11-14 02:47:05 -0300

    Add validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,4 +5,11 @@ class Reservation < ApplicationRecord
 
   validates :checkin, presence: true
   validates :paid, :left, inclusion: [true, false]
+  validate :can_reservate?, on: :create
+
+  private
+
+  def can_reservate?
+    errors.add(:vehicle_id, 'has already reserved') if vehicle.reservations.exists?(left: false)
+  end
 end
