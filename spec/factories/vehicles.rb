4674c1249568610810aa5bfdd879c938f09eb495
Valter Andrei <valterandrey@gmail.com>
2022-03-26 17:42:11 -0300

    refactor mask of license_plate

--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :vehicle do
-    plate { FFaker::String.from_regexp(/[A-Z]{3}-\d{4}/) }
+    plate { FFaker::String.from_regexp(/[A-Z]{3}-[0-9][0-9A-Z][0-9]{2}/) }
   end
 end
