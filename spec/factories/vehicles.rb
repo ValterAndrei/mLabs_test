394cb1b9b564d04c6d1d5bd4fa428cc94980c48f
Valter Andrei <valterandrey@gmail.com>
2022-08-30 13:19:18 -0300

    Update vehicles.rb

--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :vehicle do
     plate { FFaker::String.from_regexp(/[A-Z]{3}-[0-9][0-9A-Z][0-9]{2}/) }
+    # plate { Faker::Base.regexify(/[A-Z]{3}-[0-9][0-9A-Z][0-9]{2}/) }
+
     # Máscara com ou sem hífen:
     # /[A-Z]{3}-?[0-9][0-9A-Z][0-9]{2}/
   end
